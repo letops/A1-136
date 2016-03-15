c6d61f31fc827ecd583f9057f38b995527c00487
letops <daniel.ortiz@grvtylabs.com>
2016-03-15 17:57:16 -0600

    Created redirect

--- MainAPP/views.py
+++ MainAPP/views.py
@@ -2,17 +2,44 @@ from django.contrib.auth.decorators import login_required
 from django.core import urlresolvers
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import .hardcode
+from . import hardcode
 
 
 # Create your views here.
 @login_required()
 def home(request):
     if request.user.step in (hardcode.STEP_UNKNOWN, hardcode.STEP_POLL):
-        return HttpResponseRedirect(urlresolvers.reverse('login'))
+        return HttpResponseRedirect(urlresolvers.reverse('poll'))
     elif request.user.step == hardcode.STEP_CANVAS:
-        return HttpResponseRedirect(urlresolvers.reverse('login'))
+        return HttpResponseRedirect(urlresolvers.reverse('canvas'))
     elif request.user.step == hardcode.STEP_DONE:
-        return HttpResponseRedirect(urlresolvers.reverse('login'))
+        return HttpResponseRedirect(urlresolvers.reverse('share'))
     else:
-        return HttpResponseRedirect(urlresolvers.reverse('login'))
+        return HttpResponseRedirect(urlresolvers.reverse('share'))
+
+
+def poll(request):
+    result = ''
+    return render(
+        request,
+        'poll.html',
+        {"result": result}
+    )
+
+
+def canvas(request):
+    result = ''
+    return render(
+        request,
+        'canvas.html',
+        {"result": result}
+    )
+
+
+def share(request):
+    result = ''
+    return render(
+        request,
+        'share.html',
+        {"result": result}
+    )
